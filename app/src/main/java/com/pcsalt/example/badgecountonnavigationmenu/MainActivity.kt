a9088ba40b46fc2aa11e9108041de4202be88da7
krrishnaaaa <krishna@pcsalt.com>
2020-05-09 13:10:50 +0530

    displaying badge in drawer icon

--- app/src/main/java/com/pcsalt/example/badgecountonnavigationmenu/MainActivity.kt
+++ app/src/main/java/com/pcsalt/example/badgecountonnavigationmenu/MainActivity.kt
@@ -1,22 +1,29 @@
 package com.pcsalt.example.badgecountonnavigationmenu
 
 import android.os.Bundle
-import android.view.Menu
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.navigation.NavigationView
+import com.pcsalt.example.badgecountonnavigationmenu.badgeview.BadgeDrawerArrowDrawable
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
+
+    private lateinit var mDrawerToggle: ActionBarDrawerToggle
+    private lateinit var badgeDrawerArrowDrawable: BadgeDrawerArrowDrawable
+
+    private var count: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,9 +32,9 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         val fab: FloatingActionButton = findViewById(R.id.fab)
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        fab.setOnClickListener {
+            count += 1
+            setBadgeCount(count)
         }
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
@@ -41,10 +48,39 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        mDrawerToggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolbar, R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+        drawerLayout.addDrawerListener(mDrawerToggle)
+        mDrawerToggle.syncState()
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    private fun setBadgeNavigationIcon() {
+        supportActionBar?.themedContext?.let {
+            if (!::badgeDrawerArrowDrawable.isInitialized) {
+                badgeDrawerArrowDrawable = BadgeDrawerArrowDrawable(it)
+            }
+            mDrawerToggle.drawerArrowDrawable = badgeDrawerArrowDrawable
+            badgeDrawerArrowDrawable.backgroundColor =
+                ContextCompat.getColor(this, R.color.badge_color)
+        }
+    }
+
+    private fun setBadgeCount(count: Int) {
+        setBadgeNavigationIcon()
+        if (count % 7 == 0) {
+            badgeDrawerArrowDrawable.setEnabled(false)
+            badgeDrawerArrowDrawable.setText(null)
+        } else {
+            badgeDrawerArrowDrawable.setEnabled(true)
+            badgeDrawerArrowDrawable.setText("$count")
+        }
     }
 }
